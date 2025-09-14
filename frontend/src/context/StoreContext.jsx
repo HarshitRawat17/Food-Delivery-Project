5ccd83755b2ab3849b72ed4e190069cb4a252f9d
HarshitRawat17 <rawatharshit465@gmail.com>
2025-09-14 16:37:42 +0530

    Initial commit - Food Delivery project

---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    if (cartItems[itemId] > 1) {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    }
+    else {
+    // remove item completely when count reaches 0
+    setCartItems((prev) => {
+      const newCart = { ...prev };
+      delete newCart[itemId];
+      return newCart;
+    });
+  }
+
+  };
+  useEffect(()=>{
+      console.log(cartItems);
+  },[cartItems])
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
