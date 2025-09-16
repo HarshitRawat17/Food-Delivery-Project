c8746a1bfb151ba71c8c5d032958b616b6fd0545
HarshitRawat17 <rawatharshit465@gmail.com>
2025-09-16 08:13:57 +0530

    Add my new and updated files.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import { assets } from '../assets/assets'
+import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
 
 export const Navbar = ({setShowLogin}) => {
